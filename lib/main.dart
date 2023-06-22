7def44198278a29a1d36d02068fa744134d46581
otobit-hiten <hiten@otobit.in>
2023-06-22 17:48:42 +0530

    sharedPreference

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_note_app/noteModel.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Notes',
-    theme: ThemeData(
-      primarySwatch: Colors.red,
-    ),
-    initialRoute: '/',
-    routes: {
-      '/': (context) => const HomePage(),
-    },
-  ));
-}
+void main() => runApp(const MyApp());
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool isLoading = true;
-  List<Map<String, dynamic>> data = [];
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Flutter Application',
+      theme: ThemeData(
+        // This is the theme of your application.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
   }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key,}) : super(key: key);
 
-  final TextEditingController titleController = TextEditingController();
-  final TextEditingController descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('CRUD'),
-          backgroundColor: Colors.black,
-
-        ),
-        body: isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : data.isEmpty
-                ? const Center(child: Text("No Data Available!!!"))
-                : ListView.builder(
-                    itemCount: data.length,
-                    itemBuilder: (context, index) => Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        color: index % 2 == 0
-                            ? Colors.white
-                            : Colors.white,
-                        child: ListTile(
-                            onLongPress: () => delete(data[index]['id']),
-                            title: Text(data[index]['title'], style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w400,color: Colors.black)),
-                            subtitle: Text(data[index]['description'], style: const TextStyle(fontSize: 16,color: Colors.black, fontWeight: FontWeight.w300)),
-                            trailing: SizedBox(
-                                child: IconButton(
-                                    icon: const Icon(Icons.edit, color: Colors.black),
-                                    onPressed: () =>
-                                        openDialog(data[index]['id'])))))),
-        backgroundColor: Colors.black,
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.white,
-            onPressed: () {
-              openDialog(null);
-            },
-            child: const Icon(Icons.add,color: Colors.black,)));
-  }
-
-  void delete(int id) async {
-    await DbHelper.deleteItem(id);
-    getData();
-  }
-
-  openDialog(int? id) {
-    showDialog<void>(
-      context: context,
-      // false = user must tap button, true = tap outside dialog
-      builder: (BuildContext dialogContext) {
-        return AlertDialog(
-          title: const Text('Add Note'),
-          content: SizedBox(
-              height: 120,
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                      controller: titleController,
-                      decoration: const InputDecoration(labelText: 'Title')),
-                  TextField(
-                      controller: descriptionController,
-                      decoration:
-                          const InputDecoration(labelText: 'description'))
-                ],
-              )),
-          actions: <Widget>[
-            MaterialButton(
-              child: const Text('Add'),
-              onPressed: () async {
-                if (id == null) {
-                  await addData();
-                } else {
-                  await updateData(id);
-                }
-                titleController.text = '';
-                descriptionController.text = '';
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+      appBar: AppBar(
+        title: const Text('SharedPref'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
     );
-  }
-
-  void getData() async {
-    final allData = await DbHelper.getItems();
-    setState(() {
-      data = allData;
-      isLoading = false;
-    });
-  }
-
-  Future<void> addData() async {
-    await DbHelper.createItem(titleController.text, descriptionController.text);
-    getData();
-  }
-
-  Future<void> updateData(int id) async {
-    await DbHelper.updateItem(
-        id, titleController.text, descriptionController.text);
-    getData();
   }
 }
